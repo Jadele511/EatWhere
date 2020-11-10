0a8b163d4c0096d46800ae9dc4612ee58d0c961e
Jade Le <baongocle1991@gmail.com>
2020-11-10 22:12:53 +0000

    created CRUD model and test users

---
+++ crud.py
@@ -0,0 +1,40 @@
+"""CRUD operations."""
+
+from model import db, User, connect_to_db
+
+def create_user(email, password):
+    """Create and return a new user."""
+
+    user = User(email=email, password=password)
+
+    db.session.add(user)
+    db.session.commit()
+
+    return user
+
+
+def get_all_users():
+    """Return all users."""
+    return User.query.all()
+
+
+def get_user_by_id(user_id):
+    """Return user by id."""
+    return User.query.get(user_id)
+
+
+def get_user_by_email(email):
+    """ Return user by email """
+
+    return User.query.filter(User.email == email).first()
+
+
+def password_match(email, password):
+    """Check if password matches email."""
+    return User.query.filter(User.email == email,
+                             User.password == password).first()
+
+
+if __name__ == '__main__':
+    from server import app
+    connect_to_db(app)
---
+++ seed_database.py
@@ -0,0 +1,23 @@
+"""Script to seed database."""
+
+import os
+import json
+from random import choice, randint
+from datetime import datetime
+
+import crud
+import model
+import server
+
+os.system('dropdb eatwhere')
+os.system('createdb eatwhere')
+
+model.connect_to_db(server.app)
+model.db.create_all()
+
+
+for n in range(10):
+    email = f'user{n}@test.com'
+    password = 'test'
+
+    user = crud.create_user(email, password)
