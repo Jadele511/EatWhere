205b0354189d2fd4509b2028e0694234ae551970
Jade Le <baongocle1991@gmail.com>
2020-11-15 23:29:23 -0800

    removed search button, added search on change

--- static/js/restaurant-result.js
+++ static/js/restaurant-result.js
@@ -1,47 +1,49 @@
 "use strict";
 
 let resultIndex = 0;
-let resultYelp = []
+let resultYelp = [];
 
-$('#next').on('click', () => {
+$("#nextBtn").on("click", () => {
   resultIndex++;
   resultIndex %= 5;
   showRes();
-})
+});
 
-function showRes(){
-    $('#search-result>img').attr('src', `${resultYelp[resultIndex].image_url}`)
-    $('#res-name').html(`${resultYelp[resultIndex].name}`)
-    $('#rating').html(`${resultYelp[resultIndex].rating}`)
-    $('#review-count').html(`${resultYelp[resultIndex].review_count} reviews `)
-    $('#price').html(`${resultYelp[resultIndex].price}`)
-    $('#categories').html(`${resultYelp[resultIndex].categories[0].title}`)
-    $('#address').html(`${resultYelp[resultIndex].location.display_address}`)
-    $('#res-details').attr('action', `${resultYelp[resultIndex].url}`)
-    $('#res-details>input').attr('type', 'submit')
+function showRes() {
+  $("#search-result>img").attr("src", `${resultYelp[resultIndex].image_url}`);
+  $("#res-name").html(`${resultYelp[resultIndex].name}`);
+  $("#rating").html(`${resultYelp[resultIndex].rating}`);
+  $("#review-count").html(`${resultYelp[resultIndex].review_count} reviews `);
+  $("#price").html(`${resultYelp[resultIndex].price}`);
+  $("#categories").html(`${resultYelp[resultIndex].categories[0].title}`);
+  $("#address").html(`${resultYelp[resultIndex].location.display_address}`);
+  $("#res-details").attr("href", `${resultYelp[resultIndex].url}`);
+  $("#res-details").attr("style", "display: block");
 }
 
 
-$('#search-form').on('submit', (evt) => {
+function onChange(evt) {
   evt.preventDefault();
 
-  // Get user input from a form
   const formData = {
-    location: $('#search-location').val(),
-    categories: $('#search-categories').val(),
-    price: $('#search-price').val(),
-    open_now: $('#search-open_now').val(),
-    sort_by: $('#search-sort_by').val()
+    location: $("#search-location").val(),
+    categories: $("#search-categories").val(),
+    price: $("#search-price").val(),
+    sort_by: $("#search-sort-by").val(),
   };
-    // const formData = $('#search-form').serialize()
 
-  // Send formData to the server (becomes a query string)
-  $.get('/restaurants-search.json', formData, (res) => {
+  $.get("/restaurants-search.json", formData, (res) => {
     // Display response from the server
-    resultYelp = res.businesses; 
+    resultYelp = res.businesses;
     showRes();
+    $("#nextBtn").attr("style", "display: block");
+
   });
-});
+}
+$("#search-location").on('change', onChange);
+$("#search-categories").on('change', onChange);
+$("#search-price").on('change', onChange);
+$("#search-sort-by").on('change', onChange);
 
 
 
