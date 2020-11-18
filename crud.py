46c51beb74647624c4b96152c39fe3298ada2cdc
Jade Le <baongocle1991@gmail.com>
2020-11-18 06:09:32 +0000

    created restaurant, like

--- crud.py
+++ crud.py
@@ -1,9 +1,9 @@
 """CRUD operations."""
 
-from model import db, User, connect_to_db
+from model import db, User, Restaurant, Like, connect_to_db
 
 
-def create_user(email, password):
+def create_user(email, password=None):
     """Create and return a new user."""
 
     user = User(email=email, password=password)
@@ -13,16 +13,44 @@ def create_user(email, password):
 
     return user
 
+def create_restaurant(yelp_id):
+    """Create and return a new restaurant."""
+
+    res = Restaurant(yelp_id=yelp_id)
+
+    db.session.add(res)
+    db.session.commit()
+
+    return res
+
+
+def create_like(user, res, is_liked=False):
+    """ Change like attribute to true """
+
+    like = Like(user=user, res=res, is_liked=is_liked)
+
+    db.session.add(like)
+    db.session.commit()
+
+    return like
+    
 
 def get_all_users():
     """Return all users."""
     return User.query.all()
+
+def get_all_restaurants():
+    """ Return all restaurants"""
+    return Restaurant.query.all()
 
 
 def get_user_by_id(user_id):
     """Return user by id."""
     return User.query.get(user_id)
 
+def get_restaurant_by_id(yelp_id):
+    """Return restaurant by id."""
+    return Restaurant.query.get(yelp_id)
 
 def get_user_by_email(email):
     """ Return user by email """
@@ -33,7 +61,11 @@ def get_user_by_email(email):
 def password_match(email, password):
     """Check if password matches email."""
     return User.query.filter(User.email == email,
-                             User.password == password).first()
+                             User.password == password).first() != None
+
+def email_exist(email):
+    """ Check if email matches"""
+    return User.query.filter(User.email == email).first() != None
 
 
 if __name__ == '__main__':
