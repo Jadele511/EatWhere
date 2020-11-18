8dc8a4307af85a20c31eb824d23becebe08c2499
Jade Le <baongocle1991@gmail.com>
2020-11-18 22:49:21 +0000

    removed test users

--- seed_database.py
+++ seed_database.py
@@ -3,7 +3,6 @@
 import os
 import json
 from random import choice, randint
-from datetime import datetime
 
 import crud
 import model
@@ -16,9 +15,9 @@ model.connect_to_db(server.app)
 model.db.create_all()
 
 
-for n in range(10):
-    # user_name = f'test{n}'
-    email = f'user{n}@test.com'
-    password = 'test'
+# for n in range(10):
+#     # user_name = f'test{n}'
+#     email = f'user{n}@test.com'
+#     password = 'test'
 
-    user = crud.create_user(email, password)
+#     user = crud.create_user(email, password)
